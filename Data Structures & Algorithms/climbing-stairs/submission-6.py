c077e5e55f1681cdb9b8924341f6439b5b6c1fb7
Christian Peleck <christianpeleck10@gmail.com>
2026-08-06 16:15:16 -0500

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        stairs = {
+            1: 1,
+            2: 2,
+            3: 3,
+        }
+        if n <= 3:
+            return n
+        for i in range(4, n+1):
+            stairs[i] = stairs[i-1] + stairs[i- 2]
+        return stairs[n]
+        
